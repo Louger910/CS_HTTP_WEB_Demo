34d1bbb1298103eabeaf41a39f7b53eb1df64c5e
student <student@DESKTOP-L3ET5DL>
2020-03-10 19:32:28 +0200

    try http request

--- HTTP_WEB_Demo/Program.cs
+++ HTTP_WEB_Demo/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,14 @@ namespace HTTP_WEB_Demo
     {
         static void Main(string[] args)
         {
+            HttpWebRequest reqw =
+                (HttpWebRequest)HttpWebRequest.Create("https://www.fxclub.org/markets/index/nq/");
+            HttpWebResponse resp = (HttpWebResponse)reqw.GetResponse(); //создаем объект отклика
+            StreamReader sr = new StreamReader(resp.GetResponseStream(), Encoding.Default);
+            //создаем поток для чтения отклика
+            Console.WriteLine(sr.ReadToEnd());
+            //вывести на экран все, что читается
+            sr.Close();
         }
     }
 }
