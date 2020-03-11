b1a078dfa5fcafd93cbf8617cb91b483caa08d5a
student <student@DESKTOP-L3ET5DL>
2020-03-11 20:28:10 +0200

    parse virus wiki

--- HTTP_WEB_Demo/Program.cs
+++ HTTP_WEB_Demo/Program.cs
@@ -21,57 +21,89 @@ namespace HTTP_WEB_Demo
         }
 
         async static void DoTask() {
+            
             HttpWebRequest reqw =
-                (HttpWebRequest)HttpWebRequest.Create("https://www.fxclub.org/markets/index/nq/");
+                (HttpWebRequest)HttpWebRequest.Create("https://ru.wikipedia.org/wiki/%D0%92%D1%81%D0%BF%D1%8B%D1%88%D0%BA%D0%B0_COVID-19");
+            HttpWebResponse resp = (HttpWebResponse)reqw.GetResponse(); //создаем объект отклика
+            StreamReader sr = new StreamReader(resp.GetResponseStream(), Encoding.Default);
+
+            var parser = new HtmlParser();
+            var document = parser.ParseDocument(resp.GetResponseStream());
+            //Console.WriteLine(document.DocumentElement.OuterHtml);
+            IElement tableElement =
+                document.QuerySelector("h3:has(> span#Распространение_по_странам_и_территориям) + table > tbody");
+            //Console.WriteLine(tableElement.QuerySelector("tbody").InnerHtml);
+            int count = 0;
+            int totalInfected = 0;
+            int totalDead = 0;
+            int totalRecovered = 0;
+            var rows = tableElement.QuerySelectorAll("tr");
+            foreach (var item in rows)
+            {
+                if (count != 0 && count != rows.Count() - 1)
+                {
+                    try
+                    {
+                        if (!item.Children[0].InnerHtml.Contains("Макао"))
+                        {
+                            totalInfected +=
+                            (item.Children[1].InnerHtml != "") ? Int32.Parse(item.Children[1].InnerHtml) : 0;
+                            totalDead +=
+                                (item.Children[3].InnerHtml != "") ? Int32.Parse(item.Children[3].InnerHtml) : 0;
+                            totalRecovered +=
+                                (item.Children[4].InnerHtml != "") ? Int32.Parse(item.Children[4].InnerHtml) : 0;
+                        }
+                    }
+                    catch (Exception)
+                    {
+
+                        // throw;
+                    }
+                    // Console.WriteLine(item.Children[1].InnerHtml);
+                    // Console.WriteLine();
+                }
+                
+                count++;
+            }
+            Console.WriteLine(totalInfected);
+            Console.WriteLine(totalDead);
+            Console.WriteLine(totalRecovered);
+
+            Console.WriteLine(((double)totalDead / (double)totalInfected) * 100d);
+            Console.WriteLine(((double)totalRecovered / (double)totalInfected) * 100d);
+
+            sr.Close();
+
+            /*
+            // https://www.fxclub.org/markets/index/nq/
+            HttpWebRequest reqw =
+                (HttpWebRequest)HttpWebRequest.Create("https://ru.wikipedia.org/wiki/%D0%92%D1%81%D0%BF%D1%8B%D1%88%D0%BA%D0%B0_COVID-19");
             HttpWebResponse resp = (HttpWebResponse)reqw.GetResponse(); //создаем объект отклика
             StreamReader sr = new StreamReader(resp.GetResponseStream(), Encoding.Default);
             //создаем поток для чтения отклика
             // Console.WriteLine(sr.ReadToEnd());
-            string html = sr.ReadToEnd();
-            //var parser = new HtmlParser();
-            //var document = parser.ParseDocument(resp.GetResponseStream());
-            /* foreach (IElement element in document.QuerySelectorAll(".grid-7"))
-            {
-                Console.WriteLine(element);
-            } */
+            //string html = sr.ReadToEnd();
 
-            var config = Configuration.Default.WithJavaScript();
-            //Create a new context for evaluating webpages with the given config
-            var context = BrowsingContext.New(config);
+            var parser = new HtmlParser();
+            var document = parser.ParseDocument(resp.GetResponseStream());
+            //Console.WriteLine(document.DocumentElement.OuterHtml);
+            IElement tableElement =
+                document.QuerySelector("h3:has(> span#Распространение_по_странам_и_территориям) + table");
+            Console.WriteLine(tableElement.InnerHtml);
 
-            var document = await context.OpenAsync(req => req.Content(html));
-
-            Console.WriteLine(document.DocumentElement.OuterHtml);
+            sr.Close();
+             
+             */
 
             // 1
             /*IElement tableElement =
-                document.QuerySelector(".grid-7 > .fx-live-quotes-list");
+                // document.QuerySelector(".grid-7 > .fx-live-quotes-list");
+                document.QuerySelector(".grid-7");
             Console.WriteLine(tableElement.InnerHtml);*/
             /* foreach (var item in tableElement.ChildNodes)
             {
                 Console.WriteLine(item);
             } */
-
-
-            // 2
-            /*IHtmlCollection<IElement> rowElements =
-                document.QuerySelectorAll(".grid-7 > .fx-live-quotes-list");
-            foreach (IElement rowElement in rowElements)
-            {
-                Console.WriteLine(rowElement);
-            }*/
-
-            /* foreach (IElement rowElement in tableElement.ChildNodes)
-            {
-                Console.WriteLine(rowElement);
-            } */
-            /* foreach (IElement rowElement in tableElement.QuerySelectorAll("div.style__row___3vaBm"))
-            {
-                Console.WriteLine(rowElement);
-            } */
-            //(".style__row___3vaBm")
-            //вывести на экран все, что читается
-            //sr.Close();
         }
     }
 }
